e4f21560970f1a05a84641a40b2f94d6a55148e6
Gon Bogner <gon@Gon-MPB.local>
2017-07-23 18:30:22 +0300

    board defenition

---
+++ board.py
@@ -0,0 +1,10 @@
+# board definition
+board = [[1 for x in range(3)] for y in range(3)] 
+for row in range(3):
+	for col in range(3):
+		board[row][col]=raw_input('Enter a move:')
+
+			
+#board function
+
+	
