66e958bd24857b9cd002d8151c0e9c679ce9b64b
Gon Bogner <gon@gon-mpb.mynet>
2017-07-24 18:38:24 +0300

    func fill_yourself

--- board.py
+++ board.py
@@ -1,9 +1,31 @@
 import algo_board
 # board definition
 board_size=int(raw_input("Enter board size:"))
+
 board = [[1 for x in range(board_size)] for y in range(board_size)] 
 for row in range(board_size):
 	for col in range(board_size):
-		board[row][col]=raw_input('Enter a move:')	
+		board[x][y]=0
+for row in range(board_size):
+	for col in range(board_size):
+		x=raw_input int(("choose row for your move:"))
+		y=raw_input int(("choose colom for your move:"))
+		move=raw_input('Enter a move:')
+		fill_yourself(board,x,y,move)
+		
 print algo_board.XmixDrix(board)		
 
+def fill_yourself(brd,x,y,move):
+	flag=1
+	while flag==1:
+		if brd[x][y]!=0:
+			print "sorry, this place is already taken"
+			print "please enter another location for your move:"
+			x=raw_input int(("choose row for your move:"))
+			y=raw_input int(("choose colom for your move:"))
+		else:
+			brd[x][y]=move
+			flag=0
+			return	
+
+	
