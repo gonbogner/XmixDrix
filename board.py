5616ba987a4d87aeca60c7b2d8b819ec50937a99
Gon Bogner <gon@Gon-MPB.local>
2017-07-25 16:33:21 +0300

    function numbers

--- board.py
+++ board.py
@@ -25,11 +25,29 @@ def print_xo(brd):
 		print "-----------"	
 
 
+
+def number(brd):
+	mone_x=0
+	mone_o=0
+	mone=0
+	for x in range(board_size):
+		for y in range(board_size):
+			if brd[x][y]=="x":
+				mone_x+=1
+			elif brd[x][y]=="o":
+				mone_o+=1
+			else:
+				mone+=1
+
+	print "the number of X is :%d" % mone_x
+	print "the number of o is :%d" % mone_o
+	print "the number of 0 is :%d" % mone			
+			
+
 import algo_board
 # board initialization
 board_size=int(raw_input("Enter board size:"))
 board = [[0 for x in range(board_size)] for y in range(board_size)] 
-
 # main loop
 for turns in range(1,board_size**2):
 	print "Turn number ", turns
@@ -37,7 +55,8 @@ for turns in range(1,board_size**2):
 	y=int(raw_input ("choose colom for your move:"))
 	move=raw_input('Enter a move:')
 	fill_yourself(board,x,y,move)
-	print_xo(board)	
+	print_xo(board)
+	number(board)	
 	res=algo_board.XmixDrix(board)
 	if (res=="x"):
 		print "X is the winner"
@@ -49,4 +68,3 @@ for turns in range(1,board_size**2):
 if algo_board.XmixDrix(board)==0:
 	print "No one won"
 		
-
